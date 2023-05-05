2b5b9e854d9eba9e0983e048a8ac5428675ff792
Aldayr Ruiz <aldayr2000@gmail.com>
2023-05-05 09:02:33 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'eu.bluece.foodandmove',
+  appName: 'Food And Move',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
